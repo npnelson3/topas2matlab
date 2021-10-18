1e92be6e2fc9ab8b29feea88e720519d7b06f3ec
npnelson3 <71889667+npnelson3@users.noreply.github.com>
2021-10-18 11:08:35 -0500

    Add files via upload

---
+++ topas2matlab_bin.m
@@ -0,0 +1,8 @@
+function [data] = topas2matlab_bin(xbins,ybins,zbins,filename)
+fileID = fopen(filename,'r+');
+binDoseDouble = fread(fileID,'double');
+inds = [1:1:numel(binDoseDouble)];
+BinMatrix = zeros(xbins,ybins,zbins);
+BinMatrix(inds) = binDoseDouble(inds);
+data = BinMatrix;
+end
